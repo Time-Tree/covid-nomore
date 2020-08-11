500271c8560fcd6a8c2de139bf2a7fce7fb31853
Denis <ciubaru.dionis@gmail.com>
2020-08-11 16:14:38 +0300

    Implement feeling feedback card for home screen

---
+++ src/screens/HomeScreen/FeelingFeedbackCard.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+
+import { RoundedCard, RoundedButton } from '../../components';
+import FeelingFeedbackSelect from './FeelingFeedbackSelect';
+
+const FeelingFeedbackCard = ({ selectedFeedback, onFeedback }) => {
+  const icon = selectedFeedback ? (
+    <Icon name="check" size={22} color="#fff" style={styles.btnIcon} />
+  ) : null;
+
+  const buttonColor = selectedFeedback ? '#00de66' : '#00c0ff';
+
+  return (
+    <RoundedCard style={styles.card}>
+      <Text style={styles.header}>How are you feeling today?</Text>
+      <FeelingFeedbackSelect
+        selectedFeedback={selectedFeedback}
+        onFeedback={onFeedback}
+      />
+      <View style={styles.btnWrapper}>
+        <RoundedButton
+          title="CHECK VITALS"
+          containerStyle={styles.buttonContainer}
+          buttonStyle={{ ...styles.button, backgroundColor: buttonColor }}
+          onPress={() => {}}
+          iconRight={true}
+          icon={icon}
+        />
+      </View>
+    </RoundedCard>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    padding: 16,
+    marginTop: 16,
+    marginLeft: 16,
+    marginRight: 16
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  btnWrapper: {
+    alignItems: 'center',
+    marginBottom: 8
+  },
+  btnIcon: {
+    position: 'absolute',
+    right: 16
+  }
+});
+
+export default FeelingFeedbackCard;
