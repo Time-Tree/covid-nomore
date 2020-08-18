f7e9e925a46a0c417e2c2c71d412b1069cf94702
Denis <ciubaru.dionis@gmail.com>
2020-08-18 16:48:28 +0300

    Create a header component with back button

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import RoundedCard from './RoundedCard';
 import AppLogo from './AppLogo';
 import RoundedButton from './RoundedButton';
+import ScreenHeader from './ScreenHeader';
 
-export { RoundedCard, AppLogo, RoundedButton };
+export { RoundedCard, AppLogo, RoundedButton, ScreenHeader };
